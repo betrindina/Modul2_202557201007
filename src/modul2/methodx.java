35884ff0da7d1d70cb9114c83c138e7ad9ca53f8
betrindina <betrindina@gmail.com>
2026-05-06 12:03:12 +0700

    Method volume

---
+++ src/modul2/methodx.java
@@ -0,0 +1,26 @@
+package modul2;
+/*
+Class ini berisi semua method (volume & luas)
+Akan dipanggil oleh file lain (percobaan2 & percobaan3)
+*/
+public class methodx {
+    //=== METHOD VOLUME ===
+    //Method menghitung volume tabung
+    double volume_tabung (double d, double t) {
+    double r = d / 2;       //jari2
+    return Math.PI * r * r * t;
+}
+    //Method menghitung volume kerucut
+    double volume_kerucut (double d, double t){
+        double r = d / 2;
+        return (1.0 / 3) * Math.PI * r * r * t;
+    }
+    //Method menghitung volume bola
+    double volume_bola(double d){
+        double r = d / 2;
+        return(4.0 / 3) * Math.PI * r * r * r;
+    }
+    
+    }
+    
+
